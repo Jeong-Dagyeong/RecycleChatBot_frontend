d28ba77b28a5922113b1654db4b8677b7e9b8899
dagyeong Jeong <jdk09140@gmail.com>
2024-07-17 16:05:33 +0900

    Feat : tailwindcss 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+// import ChatBot from 'react-chatbotify';
+
+// 타입 정의
+// type ChatBotOptions = {
+//     chatWindowStyle: string;
+// };
+
+// 옵션 객체
+// const options: ChatBotOptions = {
+//     chatWindowStyle: `width: 100%`,
+// };
 
 function App() {
     return (
         <div className="App">
-            <p>heool</p>
+            {/* <ChatBot options={options} /> */}
+            <h1 className="text-red-700">안녕</h1>
+            <p className="text-red-700">잘 지내니</p>
+            <h1 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">hello Tailwind!</h1>
         </div>
     );
 }
