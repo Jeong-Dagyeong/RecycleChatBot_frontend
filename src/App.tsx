6e9c3436f03963b07c87889b6d421bfd44d6a5b6
dagyeong Jeong <jdk09140@gmail.com>
2024-07-18 17:47:50 +0900

    Feat : chatbot checkbox 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-// import ChatBot from 'react-chatbotify';
+import React from 'react';
+import ChatBot from 'react-chatbotify';
 
-// 타입 정의
-// type ChatBotOptions = {
-//     chatWindowStyle: string;
-// };
-
-// 옵션 객체
-// const options: ChatBotOptions = {
-//     chatWindowStyle: `width: 100%`,
-// };
+interface Params {
+  userInput: string; // 단일 선택값을 처리하기 위해 string 타입으로 정의합니다.
+  injectMessage: (message: string) => Promise<void>; // injectMessage를 함수 타입으로 정의합니다.
+}
 
 function App() {
-    return (
-        <div className="App">
-            {/* <ChatBot options={options} /> */}
-            <h1 className="text-red-700">안녕</h1>
-            <p className="text-red-700">잘 지내니</p>
-            <h1 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">hello Tailwind!</h1>
+  const [form, setForm] = React.useState<{ district: string }>({
+    district: '',
+  });
+
+  const options = {
+    theme: {
+      embedded: true,
+      primaryColor: '#526931',
+      secondaryColor: '#526931',
+    },
+    chatHistory: {
+      storageKey: 'example_theming',
+    },
+    header: {
+      title: <h1 className="font-semibold">Recycle ChatBot</h1>,
+      showAvatar: false,
+    },
+    footer: {
+      text: (
+        <div>
+          <span>Powered By </span>
+          <span className="font-bold">
+            <span>Recycle Team</span>
+          </span>
         </div>
-    );
+      ),
+    },
+  };
+
+  const flow = {
+    start: {
+      message: '안녕하세요! Recycle ChatBot 입니다. 서울특별시 구별 재활용품 지원정책에 대해 궁금한것이 있다면 무엇이든지 물어보세요.',
+      checkboxes: { items: ['서초구', '광진구', '도봉구', '관악구', '강남구'], max: 1 },
+      chatDisabled: true,
+      function: (params: Params) => setForm({ ...form, district: params.userInput }),
+      // path: 'ask_work_days',
+      path: 'end',
+    },
+
+    end: {
+      message: `서울특별시 ${form.district}의 결과는 다음과 같습니다.`,
+      render: (
+        <div className="text-orange-400 max-w-80 mt-4 ml-5">
+          <p>서울특별시 {form.district}의 결과는 다음과 같습니다.</p>
+          <p>페트병 10개</p>
+          <p>종량제 봉투 10L 1개</p>
+        </div>
+      ),
+    },
+  };
+
+  return (
+    <div className="App">
+      <ChatBot options={options} flow={flow} />
+    </div>
+  );
 }
 
 export default App;
